bfa78711083cf026e04ac6976f0f1b706a17fe06
Yong Su Jeong <knarrblue@gmail.com>
2022-07-03 22:52:10 +0900

    하수관로 수위 현황 데이터 호출

--- api/SeoulOpenApi.py
+++ api/SeoulOpenApi.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 import requests
 from dotenv import load_dotenv
 
@@ -18,3 +19,17 @@ class SeoulOpenApi:
 
         res = requests.get(url).json()
         return list(res['ListRainfallService']['row'])
+
+    def get_drain_pip(self, gubn: str):
+        '''
+        구분코드를 입력받아 서울시 공공데이터 api를 이용해 하수관로 수위 현황 데이터를 받음
+        :param gubn: String
+        :return: List
+        '''
+        now_datetime = datetime.now()
+        before_datetime = (now_datetime - datetime.timedelta(minutes=20)).strftime('%Y%m%d%H')
+
+        url = f'http://openapi.seoul.go.kr:8088/{self.KEY}/json/DrainpipeMonitoringInfo/1/5/{gubn}/{before_datetime}/{now_datetime.strftime("%Y%m%d%H")}'
+
+        res = requests.get(url).json()
+        return list(res['DrainpipeMonitoringInfo']['row'][::-1])
